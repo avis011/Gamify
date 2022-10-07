24c333ff546cb41bfd1303764cc69b6cdde9166f
Diwas Shrestha <114587196+diwas777777@users.noreply.github.com>
2022-10-07 16:29:20 +0545

    Converts image to cool cartoon picture
    It may not use tkinter but its a cool GUI project

---
+++ img to cartoon.py
@@ -0,0 +1,33 @@
+import cv2 # pip install opencv-python
+import matplotlib.pyplot as plt # pip install matplotlib
+
+img = cv2.imread("Path To The File Here")
+img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+plt.figure(figsize=(10,10))
+plt.imshow(img)
+plt.axis("off")
+plt.title("Original Image")
+plt.show()
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.medianBlur(gray, 5)
+plt.figure(figsize=(10,10))
+plt.imshow(gray,cmap="gray")
+plt.axis("off")
+plt.title("Grayscale Image")
+plt.show()
+
+edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+plt.figure(figsize=(10,10))
+plt.imshow(edges,cmap="gray")
+plt.axis("off")
+plt.title("Edged Image")
+plt.show()
+
+color = cv2.bilateralFilter(img, 9, 250, 250)
+cartoon = cv2.bitwise_and(color, color, mask=edges)
+plt.figure(figsize=(10,10))
+plt.imshow(cartoon,cmap="gray")
+plt.axis("off")
+plt.title("Cartoon Image")
+plt.show()
